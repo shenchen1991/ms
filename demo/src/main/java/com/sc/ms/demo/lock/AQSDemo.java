69f58ea97e2c116d58c8f29bed28b26df4032dbc
shenchen <572631358@qq.com>
2021-03-05 17:15:00 +0800

    MS

--- demo/src/main/java/com/sc/ms/demo/lock/AQSDemo.java
+++ demo/src/main/java/com/sc/ms/demo/lock/AQSDemo.java
@@ -1,6 +1,7 @@
 package com.sc.ms.demo.lock;
 
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.LockSupport;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -12,7 +13,8 @@ import java.util.concurrent.locks.ReentrantLock;
 public class AQSDemo {
 
     public static void main(String[] args) {
-         Lock lock = new ReentrantLock();
+        Lock lock = new ReentrantLock();
+//        LockSupport.pa
 
     }
 
