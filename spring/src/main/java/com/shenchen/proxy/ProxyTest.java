e3bcb14ba9eb8d05323eb856c6df6298b308552b
shenchen <572631358@qq.com>
2021-04-22 10:35:40 +0800

    MS

--- spring/src/main/java/com/shenchen/proxy/ProxyTest.java
+++ spring/src/main/java/com/shenchen/proxy/ProxyTest.java
@@ -11,14 +11,29 @@ import java.lang.reflect.Proxy;
 public class ProxyTest {
 
     public static void main(String[] args) {
-        SubObject subObject = (SubObject) Proxy.newProxyInstance(ProxyTest.class.getClassLoader(), new Class<?>[]{SubObject.class}, new InvocationHandler() {
-            @Override
-            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                System.out.println(method.getName());
-                return null;
-            }
-        });
+//        SubObject subObject = (SubObject) Proxy.newProxyInstance(ProxyTest.class.getClassLoader(), new Class<?>[]{SubObject.class}, new InvocationHandler() {
+//            @Override
+//            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//                System.out.println(method.getName());
+//                return null;
+//            }
+//        });
+//
+//        subObject.sayHello("test");
+        double x = 1.85;
+        double y = 4.30;
+        double z = 5.21;
+        double total = 100;
+        double payX = 100 / x;
+        double payY = 100 / y;
+        double payZ = 100 / z;
 
-        subObject.sayHello("test");
+        double totalPay = payX + payY + payZ;
+
+        System.out.println("x = " + payX);
+        System.out.println("y = " + payY);
+        System.out.println("z = " + payZ);
+        System.out.println(totalPay);
+
     }
 }
